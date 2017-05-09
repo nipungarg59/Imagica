559d9239c8a6ae9b96d27aeb235ae70e3dcc7b52
nipungarg59 <nipungarg59@gmail.com>
2017-05-10 02:31:42 +0530

    UPDATED NAME

--- Imagica/settings.py
+++ Imagica/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = getKey()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getDebugState()
 
-ALLOWED_HOSTS = ['127.0.0.1','imagica.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','imagicaa.herokuapp.com']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
