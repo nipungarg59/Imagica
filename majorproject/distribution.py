5eee0f5f3f59abfde5f5b6c885459f30a5be3f9b
nipungarg59 <nipungarg59@gmail.com>
2017-12-13 09:32:06 +0530

    About us

--- majorproject/distribution.py
+++ majorproject/distribution.py
@@ -1,69 +1,69 @@
 from __future__ import division
-import nltk
-from nltk.corpus import wordnet as wn
-from nltk.corpus import brown
 import math
 import numpy as np
 import sys
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus import brown
 from .param import *
 
 
 def best_syn_pair(word_1, word_2):
     max_sim = -1.0
-    synsets_1 = wn.synsets(word_1)
-    synsets_2 = wn.synsets(word_2)
-    if len(synsets_1) == 0 or len(synsets_2) == 0:
+    synonym_sets_1 = wn.synsets(word_1)
+    synonym_sets_2 = wn.synsets(word_2)
+    if len(synonym_sets_1) == 0 or len(synonym_sets_2) == 0:
         return None, None
     else:
         max_sim = -1.0
         best_pair = None, None
-        for synset_1 in synsets_1:
-            for synset_2 in synsets_2:
-               sim = wn.path_similarity(synset_1, synset_2)
+        for synonym_set_1 in synonym_sets_1:
+            for synonym_set_2 in synonym_sets_2:
+               sim = wn.path_similarity(synonym_set_1, synonym_set_2)
                if sim > max_sim:
                    max_sim = sim
-                   best_pair = synset_1, synset_2
+                   best_pair = synonym_set_1, synonym_set_2
         return best_pair
 
 
 
-def length_dist(synset_1, synset_2):
-    l_dist = sys.maxint
-    if synset_1 is None or synset_2 is None: 
+def length_wise_distribution(synonym_set_1, synonym_set_2):
+    length_distribution = sys.maxint
+    if synonym_set_1 is None or synonym_set_2 is None: 
         return 0.0
-    if synset_1 == synset_2:
-        l_dist = 0.0
+    if synonym_set_1 == synonym_set_2:
+        length_distribution = 0.0
     else:
-        wset_1 = set([str(x.name()) for x in synset_1.lemmas()])        
-        wset_2 = set([str(x.name()) for x in synset_2.lemmas()])
-        if len(wset_1.intersection(wset_2)) > 0:
-            l_dist = 1.0
+        word_set_1 = set([str(x.name()) for x in synonym_set_1.lemmas()])        
+        word_set_2 = set([str(x.name()) for x in synonym_set_2.lemmas()])
+        if len(word_set_1.intersection(word_set_2)) > 0:
+            length_distribution = 1.0
         else:
-            l_dist = synset_1.shortest_path_distance(synset_2)
-            if l_dist is None:
-                l_dist = 0.0
-    return math.exp(-ALPHA * l_dist)
+            length_distribution = synonym_set_1.shortest_path_distance(synonym_set_2)
+            if length_distribution is None:
+                length_distribution = 0.0
+    return math.exp(-ALPHA * length_distribution)
 
-def hierarchy_dist(synset_1, synset_2):
+def hierarchial_wise_distribution(synonym_set_1, synonym_set_2):
     h_dist = sys.maxint
-    if synset_1 is None or synset_2 is None: 
+    if synonym_set_1 is None or synonym_set_2 is None: 
         return h_dist
-    if synset_1 == synset_2:
-        h_dist = max([x[1] for x in synset_1.hypernym_distances()])
+    if synonym_set_1 == synonym_set_2:
+        h_dist = max([x[1] for x in synonym_set_1.hypernym_distances()])
     else:
-        hypernyms_1 = {x[0]:x[1] for x in synset_1.hypernym_distances()}
-        hypernyms_2 = {x[0]:x[1] for x in synset_2.hypernym_distances()}
-        lcs_candidates = set(hypernyms_1.keys()).intersection(
+        hypernyms_1 = {x[0]:x[1] for x in synonym_set_1.hypernym_distances()}
+        hypernyms_2 = {x[0]:x[1] for x in synonym_set_2.hypernym_distances()}
+        subnumers = set(hypernyms_1.keys()).intersection(
             set(hypernyms_2.keys()))
-        if len(lcs_candidates) > 0:
+        if len(subnumers) > 0:
             lcs_dists = []
-            for lcs_candidate in lcs_candidates:
+            for subnumer in subnumers:
                 lcs_d1 = 0
-                if hypernyms_1.has_key(lcs_candidate):
-                    lcs_d1 = hypernyms_1[lcs_candidate]
+                if hypernyms_1.has_key(subnumer):
+                    lcs_d1 = hypernyms_1[subnumer]
                 lcs_d2 = 0
-                if hypernyms_2.has_key(lcs_candidate):
-                    lcs_d2 = hypernyms_2[lcs_candidate]
+                if hypernyms_2.has_key(subnumer):
+                    lcs_d2 = hypernyms_2[subnumer]
                 lcs_dists.append(max([lcs_d1, lcs_d2]))
             h_dist = max(lcs_dists)
         else:
@@ -73,5 +73,5 @@ def hierarchy_dist(synset_1, synset_2):
     
 def word_similarity(word_1, word_2):
     synset_pair = best_syn_pair(word_1, word_2)
-    return (length_dist(synset_pair[0], synset_pair[1]) * 
-        hierarchy_dist(synset_pair[0], synset_pair[1]))
+    return (length_wise_distribution(synset_pair[0], synset_pair[1]) * 
+        hierarchial_wise_distribution(synset_pair[0], synset_pair[1]))
